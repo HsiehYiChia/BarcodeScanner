587b0a33fa8782d7ffd1f58b6696f0b8b1497403
ychsieh <lhsieh0107@gmail.com>
2019-04-24 13:44:22 +0800

    temp finder detector

--- BarcodeDecoder.cpp
+++ BarcodeDecoder.cpp
@@ -1,8 +1,25 @@
 #include "BarcodeDecoder.h"
 
+using namespace cv;
+using namespace std;
 
 namespace bd
 {
+
+vector<Finder> BarcodeDecoder::locate_finder(cv::Mat barcode_img)
+{
+    vector<KeyPoint> keypoints;
+    cv::FAST(barcode_img, keypoints, 0, false);
+    cout << keypoints.size() << endl;
+
+    for (int i = 0; i < keypoints.size(); i++)
+    {
+        for (int j = i; j < keypoints.size(); j++)
+        {
+            double distance = norm(keypoints[i].pt - keypoints[j].pt);
+        }
+    }
+}
 
 BarcodeDecoder::BarcodeDecoder()
 {
@@ -12,9 +29,11 @@ BarcodeDecoder::~BarcodeDecoder()
 {
 }
 
-std::wstring BarcodeDecoder::Decode(cv::Mat barcode_img)
+wstring BarcodeDecoder::Decode(cv::Mat barcode_img)
 {
-    return std::wstring();
+    threshold(barcode_img, barcode_img, THRESHOLD, 255, THRESH_BINARY);
+    locate_finder(barcode_img);
+    return wstring();
 }
 
 }
--- BarcodeDecoder.h
+++ BarcodeDecoder.h
@@ -3,15 +3,22 @@
 #include <string>
 #include <opencv2/opencv.hpp>
 
-#define MODULE_SIZE 4
 
 namespace bd
 {
+#define MODULE_SIZE 4
+#define THRESHOLD 200
+
+struct Finder
+{
+    cv::Rect bb;
+};
 
 class BarcodeDecoder
 {
 private:
-
+    std::vector<Finder> finder; /* There should be exactly 3 finder */
+    std::vector<Finder> locate_finder(cv::Mat barcode_img);
 public:
     BarcodeDecoder();
     ~BarcodeDecoder();
