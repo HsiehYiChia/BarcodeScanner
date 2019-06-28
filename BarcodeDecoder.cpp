966a7a5f95d14262ca716b1bc879f7b2127856af
ychsieh <lhsieh0107@gmail.com>
2019-06-28 23:04:51 +0800

    show demo image's finders

--- BarcodeDecoder.cpp
+++ BarcodeDecoder.cpp
@@ -1,25 +1,10 @@
 #include "BarcodeDecoder.h"
 
-using namespace cv;
 using namespace std;
+using namespace cv;
 
 namespace bd
 {
-
-vector<Finder> BarcodeDecoder::locate_finder(cv::Mat barcode_img)
-{
-    vector<KeyPoint> keypoints;
-    cv::FAST(barcode_img, keypoints, 0, false);
-    cout << keypoints.size() << endl;
-
-    for (int i = 0; i < keypoints.size(); i++)
-    {
-        for (int j = i; j < keypoints.size(); j++)
-        {
-            double distance = norm(keypoints[i].pt - keypoints[j].pt);
-        }
-    }
-}
 
 BarcodeDecoder::BarcodeDecoder()
 {
@@ -31,8 +16,25 @@ BarcodeDecoder::~BarcodeDecoder()
 
 wstring BarcodeDecoder::Decode(cv::Mat barcode_img)
 {
-    threshold(barcode_img, barcode_img, THRESHOLD, 255, THRESH_BINARY);
-    locate_finder(barcode_img);
+    threshold(barcode_img, barcode_img, THRESHOLD, 255, cv::THRESH_BINARY);
+    vector<Finder> finder(3);
+    /* up left finder */
+    finder[0].bb = Rect(Point(43,43), Point(98, 98));
+    /* up right finder */
+    finder[1].bb = Rect(Point(251,43), Point(306, 98));
+    /* bottom left finder */
+    finder[2].bb = Rect(Point(43,251), Point(98, 306));
+
+    vector<Mat> img(3);
+    
+    img[0] = barcode_img(finder[0].bb);
+    img[1] = barcode_img(finder[1].bb);
+    img[2] = barcode_img(finder[2].bb);
+    imshow ("up_left_finder", img[0]);
+    imshow ("up_right_finder", img[1]);
+    imshow ("bottom_left_finder", img[2]);
+    waitKey(0);
+
     return wstring();
 }
 
